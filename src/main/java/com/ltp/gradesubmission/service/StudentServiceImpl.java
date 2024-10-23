8d74c2120a08ffb2fb561a8860e0dbe4885e3961
supreeth10 <smuddu2@pdx.edu>
2024-10-22 17:29:14 -0700

    Initial commit

---
+++ src/main/java/com/ltp/gradesubmission/service/StudentServiceImpl.java
@@ -0,0 +1,50 @@
+package com.ltp.gradesubmission.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.ltp.gradesubmission.entity.Student;
+import com.ltp.gradesubmission.exception.StudentNotFoundException;
+import com.ltp.gradesubmission.repository.StudentRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+
+@AllArgsConstructor
+@Service
+public class StudentServiceImpl implements StudentService {
+
+
+    StudentRepository studentRepository;
+
+    @Override
+    public Student getStudent(Long id) {
+        Optional<Student> student = studentRepository.findById(id);
+        if(student.isPresent()) {
+            return student.get();
+        }else{
+            throw new StudentNotFoundException(id);
+        }
+    }
+
+    @Override
+    public Student saveStudent(Student student) {
+
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public void deleteStudent(Long id) {
+        studentRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Student> getStudents() {
+
+        return (List<Student>)studentRepository.findAll();
+    }
+
+
+}
---
+++ src/main/java/com/ltp/gradesubmission/web/StudentController.java
@@ -0,0 +1,48 @@
+package com.ltp.gradesubmission.web;
+
+import com.ltp.gradesubmission.entity.Student;
+import com.ltp.gradesubmission.service.StudentService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+
+    StudentService studentService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudent(@PathVariable long id){
+
+        return new ResponseEntity<>(studentService.getStudent(id),HttpStatus.OK);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Student> saveStudent(@RequestBody Student student){
+        return new ResponseEntity<>(studentService.saveStudent(student),HttpStatus.CREATED);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Student> deleteStudent(@PathVariable long id){
+        studentService.deleteStudent(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Student>> getStudents(){
+
+        return new ResponseEntity<>(studentService.getStudents(),HttpStatus.OK);
+    }
+}
+
+
+
+
